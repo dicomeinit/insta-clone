534fd16c3d986033e44e60a6d7a007245a4d553f
Diana <dicomeinit@gmail.com>
2024-02-27 00:27:07 +0200

    create user

---
+++ routers/user.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from sqlalchemy.orm.session import Session
+from routers.schemas import UserBase, UserDisplay
+from fastapi.param_functions import Depends
+from db.database import get_db
+from db import db_user
+
+router = APIRouter(
+    prefix='/user',
+    tags=['user']
+)
+
+@router.post('', response_model=UserDisplay)
+def create_user(request: UserBase, db: Session = Depends(get_db)):
+    return db_user.create_user(db, request)
